0e8fc90464acc317bd80ed8da2f28a76693976aa
Crowdbotics <team@crowdbotics.com>
2023-11-23 02:10:16 +0000

    Initial commit

---
+++ backend/withered_union_44722/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for withered_union_44722 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "withered_union_44722.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-union-44722",
+  url: "https://withered-union-44722.botics.co",
+  api: "https://withered-union-44722.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
